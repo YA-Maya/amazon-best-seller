83ab91bdf005938a0a1f70c959b1cb6066bd3996
YA-Maya <Sumeyask6@gmail.com>
2023-04-17 16:54:56 +0100

    Making the booklist and add tree

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -4,12 +4,18 @@ import bookArray from "./books";
 import Book from "./book";
 
 function App() {
+
+  const getBook = (id) => {const book = bookArray.find(book => book.id === id);
+    console.log(book);}
+
+
+
   return (
     <section className="booklist">
       <EventExample />
       {bookArray.map((book, index) => {
-        const { img, title, author } = book;
-        return <Book key={index} img={img} title={title} author={author} />;
+        const { img, title, author, id} = book;
+        return <Book key={id} id={id} img={img} title={title} author={author}  getBook ={getBook}/>;
       })}
     </section>
   );
